9c524d5f7aca9a4dfb828c48fc8fbedad482cba0
Lukasz Wisniewski <lwisniewski387@gmail.com>
2018-07-03 06:52:52 +0100

    Xdebug Configuration moved to ntrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM php:7.2.6-fpm-alpine3.7
 
+ENV XDEBUG_STATUS true
 ENV PHP_FPM_CONF "development"
 ENV XDEBUG_REMOTE_HOST "192.168.0.5"
 ENV XDEBUG_IDE_KEY "PHPSTORM"
 ENV XDEBUG_PORT "7765"
 
 RUN apk update && apk add \
+        build-base \
         curl-dev \
         libzip-dev \
         freetype-dev \
@@ -13,7 +15,13 @@ RUN apk update && apk add \
         libpng-dev \
         libxml2-dev \
         gettext-dev \
-        autoconf \
-    && docker-php-ext-install -j$(nproc) mysqli pdo pdo_mysql zip gd mbstring xml curl json gettext tokenizer
+        autoconf
+    && pecl install xdebug-2.6.0 \
+    && docker-php-ext-install -j$(nproc) mysqli pdo pdo_mysql zip gd mbstring xml curl json gettext tokenizer \
+    && docker-php-ext-enable xdebug
 
-COPY conf/php.ini-$PHP_FPM_CONF /usr/local/etc/php/php.ini
+COPY entrypoint/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["php-fpm"]
---
+++ entrypoint/entrypoints.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if [ "$XDEBUG_STATUS" = true ]; then
+    RUN echo "xdebug.remote_autostart=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_enable=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_handler=dbgp" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_connect_back=0" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_log=/tmp/xdebug_remote.log" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_port=$XDEBUG_PORT" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.idekey=$XDEBUG_IDE_KEY" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
+COPY conf/php.ini-$PHP_FPM_CONF /usr/local/etc/php/php.ini
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+exec "$@"
